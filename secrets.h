7a98733c9022f682449533a6120f89659c1ab9a6
sveco86 <sveco86@gmail.com>
2026-09-15 16:26:48 +0200

    Refactor Wi-Fi and MQTT configuration to use placeholders

---
+++ secrets.h
@@ -0,0 +1,15 @@
+#pragma once
+
+// Wi-Fi pripojenie
+#define WIFI_SSID     "WIFI_SSID"
+#define WIFI_PASSWORD "WIFI_PASSWORD"
+
+// TMEP doména
+#define TMEP_DOMAIN "http://domain.tmep.cz/"
+
+/*
+ * Prihlasovacie udaje pre ElegantOTA (stranka /update).
+ */
+
+#define OTA_USERNAME "OTA_USERNAME"
+#define OTA_PASSWORD "OTA_PASSWORD"
